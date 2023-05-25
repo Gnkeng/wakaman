398d27d838d16b6609882f2686e88eedfe7e37ac
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-05-25 17:03:35 +0100

    signup

---
+++ src/components/common/input/TextInput.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+const TextInput = ({
+  id,
+  label,
+  name,
+  value,
+  placeholder,
+  onChange,
+  type,
+}) => {
+  return (
+    <div className="mb-4">
+      <label className="block  text-[17px] text-secondary  font-[500] mb-2">
+        {label}
+      </label>
+      <input
+        className={` border border-slate-300 rounded w-full py-4 px-3 text-secondary font-normal text-[17px] leading-tight focus:outline-none focus:shadow-outline placeholder:text-[17px] placeholder:font-normal focus:ring-1  ${
+          value.length > 2
+            ? "focus:ring-1 focus:ring-validColor"
+            : value.length === 0
+            ? ""
+            : "focus:ring-1 focus:ring-errorColor"
+        }`}
+        id={id}
+        placeholder={placeholder}
+        onChange={onChange}
+        name={name}
+        value={value}
+        type={type}
+      />
+    </div>
+  );
+};
+
+export default TextInput;
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,10 +1,107 @@
-import React from 'react'
-import styles from './signup.module.css'
+import React ,{useState} from 'react'
+import TextInput from '../../components/common/input/TextInput';
+import PasswordInput from '../../components/common/input/PasswordInput';
+import Button from '../../components/common/button/Button';
+// import styles from './signup.module.css'
 
 const Signup = () => {
+    const [form, setForm] = useState({
+       firstname: "",
+      lastname: "",
+      email:'',
+      password:'',
+      confirmpassword: '',
+    });
+
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(form)
+      };
+
+
+    
   return (
-    <div>Signup</div>
-  )
+    <div className="bg-mid h-screen  flex flex-row justify-center items-center  ">
+      <div className="w-[500px] h-[640px] py-5 px-10 bg-white rounded-lg ">
+        <div className="text-center">
+          <h1 className="font-bold text-[2p4x] text-dark">
+            SIGNUP TO <span className="text-brand">WAKAMAN</span>
+          </h1>
+        </div>
+
+        <form className="mt-4"  onSubmit={handleSubmit}>
+          <div>
+            <TextInput
+              label="First Name"
+              placeholder="First Name"
+              type="text"
+              id="firstname"
+              value={form.firstname}
+              onChange={(e) => {
+                setForm({ ...form, firstname: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            <TextInput
+              label="Last Name"
+              placeholder="Last Name"
+              type="text"
+              id="lastname"
+              value={form.lastname}
+              onChange={(e) => {
+                setForm({ ...form, lastname: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            <TextInput
+              label="Email"
+              placeholder="example@email.com"
+              type="text"
+              id="firstname"
+              value={form.email}
+              onChange={(e) => {
+                setForm({ ...form, email: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            <PasswordInput
+              label="Password"
+              placeholder="******"
+              type="password"
+              id="password"
+              value={form.password}
+              onChange={(e) => {
+                setForm({ ...form, password: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            <PasswordInput
+              label="Confirm Password"
+              placeholder="******"
+              type="password"
+              id="password"
+              value={form.confirmpassword}
+              onChange={(e) => {
+                setForm({ ...form, confirmpassword: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            <Button
+              type={"submit"}
+              buttonType="PRIMARY"
+              text={"Register"}
+              fullWidth={true}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Signup
