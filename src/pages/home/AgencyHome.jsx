3b04fd5824c4a30ddfc5688bfc16b183adddc968
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-06-06 13:38:37 +0100

    init

--- src/pages/home/AgencyHome.jsx
+++ src/pages/home/AgencyHome.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import Button from "../../components/common/button/Button";
 
 const AgencyHome = () => {
   return (
-    <div>AgencyHome</div>
-  )
-}
+    <div>
+      <div className="bg-[#f4f4f4]  h-screen">
+        <div className="text-center pt-4">
+          <h3 className="text-4xl font-bold">Bus Agency</h3>
+        </div>
 
-export default AgencyHome
+        <div className="flex flex-col justify-center gap-10 mt-6">
+          <Button text={"Fast Book Trips"} buttonType={"PRIMARY"} />
+          <Button text={"One Way Trips"} buttonType={"OUTLINE"} />
+          <Button text={"Go and Come Trips"} buttonType={"SECONDARY"} />
+
+          {/* <GoCameCard /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AgencyHome;
