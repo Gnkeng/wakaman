443abd8c8e6004a38ecd6cd4ec434ffca555d005
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-06-29 14:18:04 +0100

    fix errors

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,85 +1,86 @@
-import React ,{useState} from 'react'
-import TextInput from '../../components/common/input/TextInput';
-import PasswordInput from '../../components/common/input/PasswordInput';
-import Button from '../../components/common/button/Button';
+import React, { useState } from "react";
+import TextInput from "../../components/common/input/TextInput";
+import PasswordInput from "../../components/common/input/PasswordInput";
+import Button from "../../components/common/button/Button";
 import { useNavigate } from "react-router-dom";
-import SelectInput from '../../components/common/input/SelectInput';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth,db } from '../../firebase-config';
-import { getDoc, collection, addDoc,getDocs,query ,where} from "firebase/firestore";
-import { onAuthStateChanged } from 'firebase/auth';
-import { useDispatch,useSelector } from 'react-redux';
+import SelectInput from "../../components/common/input/SelectInput";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../../firebase-config";
+import {
+  getDoc,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
 // import { customerInfo } from '../../store/customer/customerSlice';
-import { agencyInfo } from '../../store/agency/agencySlice';
-
-
+import { agencyInfo } from "../../store/agency/agencySlice";
 
 const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  
-  const customerSlice = useSelector((state)=> state.customer)
 
+  const customerSlice = useSelector((state) => state.customer);
 
-  const [userEmail, setUserEmail] = useState('');
+  const [userEmail, setUserEmail] = useState("");
   const [presentUser, setPresentUser] = useState({});
-  
+
   const [form, setForm] = useState({
-    email:'',
-    password:'',
-    role:''
+    email: "",
+    password: "",
+    role: "",
   });
 
   // console.log(presentUser);
 
-  const [error,setError]=useState({
-    email: '',
-    password: '',
-    role:''
-  })
+  const [error, setError] = useState({
+    email: "",
+    password: "",
+    role: "",
+  });
 
   const [firebaseErr, setFirebaseErr] = useState("");
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (form.password.length < 6) {
+      return setError({
+        ...error,
+        password: "Password must be at least 6 characters",
+      });
+    }
+    if (form.role === "customer") {
+      console.log("am a customer");
+      try {
+        const customer = await signInWithEmailAndPassword(
+          auth,
+          form.email,
+          form.password
+        );
 
-   
-   
+        const customerRef = collection(db, "customers");
+        onAuthStateChanged(auth, (currentUser) => {
+          setUserEmail(currentUser.email);
+        });
 
-
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      if(form.password.length < 6){
-       return  setError({...error,password:'Password must be at least 6 characters'})
-      }
-      if(form.role === 'customer'){
-        console.log('am a customer');
         try {
-      const customer = await signInWithEmailAndPassword(
-        auth,
-        form.email,
-        form.password
-      );
-
-         const customerRef = collection(db, "customers");
-         onAuthStateChanged(auth, (currentUser) => {
-           setUserEmail(currentUser.email);
-         });
-
-         try{
-          
-           const q = query(customerRef, where("email", "==", form.email));
+          const q = query(customerRef, where("email", "==", form.email));
           //  setPresentUser(q);
-           const querySnapshot = await getDocs(q);
-           querySnapshot.forEach((doc) => {
+          const querySnapshot = await getDocs(q);
+          querySnapshot.forEach((doc) => {
             //  if (doc.data()) {
             //    setPresentUser(doc.data());
             //    console.log(presentUser);
             //  }
-            
-             // doc.data() is never undefined for query doc snapshots
-             console.log(doc.id, " => ", doc.data());
+
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
             //  dispatch(customerInfo(doc.data()));
-            if(doc.data().email === form.email){
-            // console.log(doc.data().email);
+            if (doc.data().email === form.email) {
+              // console.log(doc.data().email);
               // setPresentUser(doc.data().email);
               // dispatch(customerInfo(doc.data()));
 
@@ -87,74 +88,54 @@ const Login = () => {
               // console.log(customerSlice);
 
               navigate("/customer-home");
-
             }
+          });
+        } catch (err) {
+          console.log(err.message);
+        }
 
-           
-
-             
-
-             
-           });
-           
-
-         }catch(err){
-                  console.log(err.message);
-         }
-
-
-      // console.log(userEmail);
-     
-      }catch(error){
+        // console.log(userEmail);
+      } catch (error) {
         console.log(error.message);
         setFirebaseErr(error.message);
       }
+    } else {
+      try {
+        const agency = await signInWithEmailAndPassword(
+          auth,
+          form.email,
+          form.password
+        );
 
+        console.log(agency);
 
+        const agencyRef = collection(db, "agency");
+        onAuthStateChanged(auth, (currentUser) => {
+          setUserEmail(currentUser.email);
+        });
 
-      
-    }
-      else{
-
-        try{
-           const agency = await signInWithEmailAndPassword(
-             auth,
-             form.email,
-             form.password
-           );
-
-            const agencyRef = collection(db, "agency");
-            onAuthStateChanged(auth, (currentUser) => {
-              setUserEmail(currentUser.email);
+        console.log(userEmail);
+        if (userEmail) {
+          try {
+            const q = query(agencyRef, where("email", "==", userEmail));
+            setPresentUser(q);
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+              // doc.data() is never undefined for query doc snapshots
+              console.log(doc.id, " => ", doc.data());
+              dispatch(agencyInfo(doc.data()));
             });
-             try {
-               const q = query(agencyRef, where("email", "==", userEmail));
-               setPresentUser(q);
-               const querySnapshot = await getDocs(q);
-               querySnapshot.forEach((doc) => {
-                 // doc.data() is never undefined for query doc snapshots
-                 console.log(doc.id, " => ", doc.data());
-                 dispatch(agencyInfo(doc.data()));
-               });
-                navigate("/agency-home");
-             } catch (err) {
-               console.log(err.message);
-             }
-
-              
-
-
-        }catch(err){
-          console.log(error.message);
-          setFirebaseErr(error.message);
-
+            navigate("/agency-home");
+          } catch (err) {
+            console.log(err.message);
+          }
         }
-
+      } catch (err) {
+        console.log(error.message);
+        setFirebaseErr(error.message);
       }
-                  
-
-      
-    };
+    }
+  };
   return (
     <div className="bg-mid h-screen  flex flex-row justify-center items-center  ">
       <div className="w-[500px] h-[640px] py-5 px-10 bg-white rounded-lg ">
@@ -230,6 +211,6 @@ const Login = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
