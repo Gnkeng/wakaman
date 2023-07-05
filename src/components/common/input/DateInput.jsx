4f2ea1712b1bfff07cc6724e13289bcb5ff5dab6
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-07-05 13:14:37 +0100

    add DateInput

---
+++ src/components/common/input/DateInput.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DateInput = ({ label, width, type, onChange }) => {
+  return (
+    <div className="flex flex-col w-full" style={{ width: width }}>
+      <label>{label}</label>
+      <input type={type} onChange={onChange} />
+    </div>
+  );
+};
+
+export default DateInput;
